6725852d8eaab4c35051aedea48887064d7f1954
sanjayis004 <sanjayksharma004@gmail.com>
2024-02-06 00:00:06 +0530

    code updated

--- src/configs/index.js
+++ src/configs/index.js
@@ -4,18 +4,13 @@ module.exports = {
     "SHORT_URL":"short_url",
     "ANALYTICS":"analytics",
     "mysql": {
-        // "host": "sql6.freesqldatabase.com",
-        // "port": "3306",
-        // "user": "sql6682199",
-        // "password": "NFbD64wiuE",
-        // "database": "sql6682199",
-        // "connectionLimit": 1
-        "host":"127.0.0.1",
-      "port":"3306",
-      "user":"root",
-      "password":"Samiscool@123",
-      "database":"url_shortener_v1",
-      "connectionLimit" : 100
+        "host": "sql6.freesqldatabase.com",
+        "port": "3306",
+        "user": "sql6682199",
+        "password": "NFbD64wiuE",
+        "database": "sql6682199",
+        "connectionLimit": 1
+        
     }
 }
 
